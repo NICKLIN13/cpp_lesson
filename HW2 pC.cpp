baff1f0c913f04b5d4cb273481de7063f0e75152
andrewmaipue <63439849+andrewmaipue@users.noreply.github.com>
2023-04-25 20:59:52 +0800

    vector initialization

--- HW2 pC.cpp
+++ HW2 pC.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
 
 int main() {
-    string name;
-    cin >> name;
-    cout << "Hello, " << name << "!" << "\n";
+    // string name;
+    // cin >> name;
+    // cout << "Hello, " << name << "!" << "\n";
+
+    vector<string> list = {"Hello, ", "Nick!"};
+    for (int i = 0; i < list.size(); i++) {
+        cout << list[i] << "\n";
+    }
+
+    vector<string> list2(5, "Wow");
+    for (int i = 0; i < list2.size(); i++) {
+        cout << list2[i] << "\n";
+    }
     return 0;
 }
