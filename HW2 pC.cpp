9a9b71a418e9cc74edec33c0c6ed20cd68921413
andrewmaipue <63439849+andrewmaipue@users.noreply.github.com>
2023-04-25 21:09:20 +0800

    nested vector

--- HW2 pC.cpp
+++ HW2 pC.cpp
@@ -12,10 +12,15 @@ int main() {
     for (int i = 0; i < list.size(); i++) {
         cout << list[i] << "\n";
     }
-
-    vector<string> list2(5, "Wow");
-    for (int i = 0; i < list2.size(); i++) {
-        cout << list2[i] << "\n";
+    cout << "\n";
+    
+    // nested vector
+    vector<vector<string>> lists(3, {"12", "ab"});
+    for (int j = 0; j < lists.size(); j++) {
+        cout << j << "\n";
+        for (int i = 0; i < lists[j].size(); i++) {
+            cout << lists[j][i] << "\n";
+        }
+        cout << "\n";
     }
-    return 0;
 }
