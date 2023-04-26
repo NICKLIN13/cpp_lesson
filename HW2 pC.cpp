d826da75b55430097e5ab740862032f63c63f871
andrewmaipue <63439849+andrewmaipue@users.noreply.github.com>
2023-04-26 23:15:13 +0800

    HW2-C: Added GO BACKWARDS

--- HW2 pC.cpp
+++ HW2 pC.cpp
@@ -24,7 +24,7 @@ bool is_valid(vector<int> p, vector<int> q) {
 }
 
 int main() {
-    DEBUG = true;
+    // DEBUG = true;
     int N = 0, M = 0, R = 0;
     cin >> N >> M;
     cin >> R;
@@ -49,6 +49,8 @@ int main() {
     // Count the valid routes of each combination
 
     int max = 0;
+
+
     // 1. Choose a route as the first one
     for (int i = 0; i < routes.size(); i++) {
 
@@ -78,6 +80,40 @@ int main() {
         max = (combination.size() > max) ? combination.size() : max;
     }
 
+    // GO BACKWARDS
+
+    // 1. Choose a route as the first one
+    // GO BACKWARDS
+    for (int i = routes.size() - 1; i >= 0; i--) {
+
+        if (DEBUG) cout << "i=" << i << "\n";
+        vector<int> combination;
+        combination.push_back(i);
+
+        // 2. Add another new route, one by one
+        // GO BACKWARDS
+        for (int j = i - 1; j >= 0; j--) {
+            // 3. Examine if the new route is valid
+                // Define is_valid() // DONE
+                // Compare the new route with the previous route
+            int previous_r = combination[combination.size() - 1];
+
+            // GO BACKWARDS
+            if (is_valid(routes[j], routes[previous_r])) {
+                combination.push_back(j);
+            }
+        }
+
+        // Check comination
+        if (DEBUG) {
+            for (int i = 0; i < combination.size(); i++) {
+                print_r(routes[combination[i]]);
+            }
+            cout << "size = " << combination.size() << "\n\n";
+        }
+
+        max = (combination.size() > max) ? combination.size() : max;
+    }
 
     // 4. Find the max count
     cout << max << "\n";
