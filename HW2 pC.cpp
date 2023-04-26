69f2fb4bbcf07bce88d06a79f6ccb524e6b7b4b5
andrewmaipue <63439849+andrewmaipue@users.noreply.github.com>
2023-04-26 18:50:21 +0800

    HW2-C: Sort routes

--- HW2 pC.cpp
+++ HW2 pC.cpp
@@ -3,6 +3,16 @@
 #include <vector>
 using namespace std;
 
+void print_routes(vector<vector<int>> routes) {
+        for (int i = 0; i < routes.size(); i++) {
+        for (int j = 0; j < routes[i].size(); j++) {
+            cout << routes[i][j] << " ";
+        }
+        cout << "\n";
+    }
+    cout << "\n";
+}
+
 int main() {
     int N = 0, M = 0, R = 0;
     cin >> N >> M;
@@ -17,14 +27,11 @@ int main() {
     // Confirm inputs // TEST
     cout << "\n";
     cout << "N=" << N << ", M=" << M << "\n";
-    for (int i = 0; i < routes.size(); i++) {
-        for (int j = 0; j < routes[i].size(); j++) {
-            cout << routes[i][j] << " ";
-        }
-        cout << "\n";
-    }
+    print_routes(routes);
 
     // Sort routes
+    sort(routes.begin(), routes.end());
+    print_routes(routes); // TEST
 
     // Count the valid routes of each combination
         // Generate all combinations?
