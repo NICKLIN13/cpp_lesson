dd3253ce2d2d0e051b17d63132d054a3d91543bd
andrewmaipue <63439849+andrewmaipue@users.noreply.github.com>
2023-04-26 18:41:16 +0800

    HW2-C: Finish pseudo codes

--- HW2 pC.cpp
+++ HW2 pC.cpp
@@ -8,20 +8,30 @@ int main() {
     cin >> N >> M;
     cin >> R;
 
-    // nested vector
+    // Read inputs
     vector<vector<int>> routes(R, {0, 0});
     for (int i = 0; i < routes.size(); i++) {
         cin >> routes[i][0] >> routes[i][1];
     }
 
+    // Confirm inputs // TEST
     cout << "\n";
-
     cout << "N=" << N << ", M=" << M << "\n";
-
     for (int i = 0; i < routes.size(); i++) {
         for (int j = 0; j < routes[i].size(); j++) {
             cout << routes[i][j] << " ";
         }
         cout << "\n";
     }
+
+    // Sort routes
+
+    // Count the valid routes of each combination
+        // Generate all combinations?
+        // Examine valid routes
+            // Define is_valid()
+
+    // Find the max count
+
+
 }
