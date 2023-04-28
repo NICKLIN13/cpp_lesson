3598ef1c93c733e9459fcef60392b5b6cbaeaddf
andrewmaipue <63439849+andrewmaipue@users.noreply.github.com>
2023-04-29 01:29:45 +0800

    HW2-C: NOT WORKING

--- HW2 pC.cpp
+++ HW2 pC.cpp
@@ -12,6 +12,15 @@ bool is_valid(array<long long, 2> p, array<long long, 2> q) {
     return (p[1] < q[1] && p[0] < q[0]);
 }
 
+void print_routes(vector<array<long long, 2>>& routes) {
+    for (long long i = 0; i < routes.size(); i++) {
+        for (long long j = 0; j < 2; j++) {
+            cout << routes[i][j] << " ";
+        }
+        cout << "\n";
+    }
+}
+
 int main() {
     std::ios_base::sync_with_stdio(false);
     std::cin.tie(nullptr);
@@ -23,6 +32,11 @@ int main() {
 
     // Read inputs
     vector<array<long long, 2>> routes(R, {0, 0});
+
+    cout << "\n";
+    print_routes(routes);
+    cout << "\n";
+    
     for (long long i = 0; i < R; i++) {
         cin >> routes[i][0] >> routes[i][1];
     }
@@ -36,7 +50,16 @@ int main() {
     // Sort routes
     sort(routes.begin(), routes.begin()+R);
 
+    cout << "\n";
+    print_routes(routes);
+    cout << "\n";
+
     long long dp[R_MAX] = {1};
+    for (long long i = 0; i < R; i++) {
+        cout << dp[i] << " ";
+    }
+    cout << "\n";
+
     for (long long i = 0; i < R; i++) {
         if (DEBUG) cout << "i=" << i << "\n";
 
@@ -46,6 +69,11 @@ int main() {
             }
         }
     }
+
+    for (long long i = 0; i < R; i++) {
+        cout << dp[i] << " ";
+    }
+    cout << "\n";
 
     // 4. Find the max count
     cout << *max_element(dp, dp+R) << "\n";
