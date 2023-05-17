accc33349f063ffd5a871018ec3b641853490960
andrewmaipue <63439849+andrewmaipue@users.noreply.github.com>
2023-05-18 06:06:40 +0800

    HW3-B: Find MAXimum spanning tree

--- HW3-B.cpp
+++ HW3-B.cpp
@@ -4,7 +4,7 @@
 #include <vector>
 #include <iomanip>      // for setw()
 
-const int Max_Weight = 1000;    // 初始化key[]時需要infinity, 以Max_Weight代替
+const int Max_Weight = -1000;    // 初始化key[]時需要infinity, 以Max_Weight代替
 
 class GraphMST{
 private:
@@ -28,7 +28,7 @@ int MinKeyExtract(int *key, bool *visited, int size){
 
     int min = Max_Weight, min_idx = 0;
     for (int i = 0; i < size; i++) {
-        if (visited[i]==false && key[i]<min) {
+        if (visited[i]==false && key[i] > min) { // MAX
             min = key[i];
             min_idx = i;
         }
@@ -51,7 +51,7 @@ void GraphMST::PrimMST(int Start){
         int u = MinKeyExtract(key, visited, num_vertex);
         visited[u] = true;
         for (int i = 0; i < num_vertex; i++) {
-            if ( visited[i]==false && AdjMatrix[u][i]!=0 && AdjMatrix[u][i]<key[i]) {
+            if ( visited[i]==false && AdjMatrix[u][i]!=0 && AdjMatrix[u][i] > key[i]) { // MAX
                 predecessor[i] = u;
                 key[i] = AdjMatrix[u][i];
             }
@@ -62,7 +62,7 @@ void GraphMST::PrimMST(int Start){
     // std::cout << std::setw(3) << "v1" << " - " << std::setw(3) << "v2"<< " : weight\n";
     // int i = (Start+1)%num_vertex;   // 若從4開始, i依序為5,6,0,1,2,3
     // while (i != Start) {
-    //     std::cout << std::setw(3) << predecessor[i] << " - " << std::setw(3) << i
+    //     std::cout << std::setw(3) << predecessor[i]+1 << " - " << std::setw(3) << i+1
     //               << " : " << std::setw(4) << AdjMatrix[predecessor[i]][i] <<"\n";
     //     i = (++i)%num_vertex;       // 到了6之後, 6+1 = 7, error:bad_access, 透過mod把7喬回0
     // }
