d9d4dc4b6f735e88d9bdcd98c707c8441a59c39a
andrewchiull <63439849+andrewchiull@users.noreply.github.com>
2023-05-21 15:07:05 +0800

    HW3-C: Basic utilies

--- HW3-C.cpp
+++ HW3-C.cpp
@@ -1,65 +1,94 @@
 // [Flood fill Algorithm - how to implement fill() in paint? - GeeksforGeeks](https://www.geeksforgeeks.org/flood-fill-algorithm-implement-fill-paint/)
 // A C++ program to implement flood fill algorithm
-#include<iostream>
+#include <iostream>
+#include <vector>
+#include <string>
+
 using namespace std;
- 
-// Dimensions of paint screen
-#define M 8
-#define N 8
- 
+
+bool debug = true;
+
+class Lava {
+private:
+    const int N, M;
+    vector<vector<char>> room;
+    void floodFillUtil(int x, int y, char prevC, char newC);
+public:
+    Lava(int N, int M): N(N), M(M) {
+        room.resize(N);
+        for (int i = 0; i < N; i++) {
+            room[i].resize(M);
+        }
+    }
+    void SetRoom(int x, int y, char C);
+    void PrintRoom();
+    void floodFill(int x, int y, char newC);
+
+};
+
+void Lava::SetRoom(int x, int y, char c) {
+    if (debug) cout << x << "," << y << " = " << c << "\n";
+    room[x][y] = c;
+    if (debug) cout << "done\n";
+
+}
+
+void Lava::PrintRoom() {
+    for (auto row: room) {
+        for (auto col: row) {
+            cout << col;
+        }
+        cout << "\n";
+    }
+}
+
 // A recursive function to replace previous color 'prevC' at  '(x, y)'
 // and all surrounding pixels of (x, y) with new color 'newC' and
-void floodFillUtil(int screen[][N], int x, int y, int prevC, int newC)
+void Lava::floodFillUtil(int x, int y, char prevC, char newC)
 {
     // Base cases
-    if (x < 0 || x >= M || y < 0 || y >= N)
+    if (x < 0 || x >= N || y < 0 || y >= M)
         return;
-    if (screen[x][y] != prevC)
+    if (room[x][y] != prevC)
         return;
-    if (screen[x][y] == newC)
+    if (room[x][y] == newC)
         return;
- 
+
     // Replace the color at (x, y)
-    screen[x][y] = newC;
- 
+    SetRoom(x, y, newC);
+
     // Recur for north, east, south and west
-    floodFillUtil(screen, x+1, y, prevC, newC);
-    floodFillUtil(screen, x-1, y, prevC, newC);
-    floodFillUtil(screen, x, y+1, prevC, newC);
-    floodFillUtil(screen, x, y-1, prevC, newC);
+    floodFillUtil(x+1, y, prevC, newC);
+    floodFillUtil(x-1, y, prevC, newC);
+    floodFillUtil(x, y+1, prevC, newC);
+    floodFillUtil(x, y-1, prevC, newC);
 }
- 
+
 // It mainly finds the previous color on (x, y) and
 // calls floodFillUtil()
-void floodFill(int screen[][N], int x, int y, int newC)
+void Lava::floodFill(int x, int y, char newC)
 {
-    int prevC = screen[x][y];
-      if(prevC==newC) return;
-    floodFillUtil(screen, x, y, prevC, newC);
+    char prevC = room[x][y];
+    if (prevC == newC) return;
+    floodFillUtil(x, y, prevC, newC);
 }
- 
+
 // Driver code
 int main()
 {
+    debug = false;
+    int N, M;
+    cin >> N >> M;
+    Lava l(N, M);
 
-    int screen[M][N] = {{1, 1, 1, 1, 1, 1, 1, 1},
-                      {1, 1, 1, 1, 1, 1, 0, 0},
-                      {1, 0, 0, 1, 1, 0, 1, 1},
-                      {1, 2, 2, 2, 2, 0, 1, 0},
-                      {1, 1, 1, 2, 2, 0, 1, 0},
-                      {1, 1, 1, 2, 2, 2, 2, 0},
-                      {1, 1, 1, 1, 1, 2, 1, 1},
-                      {1, 1, 1, 1, 1, 2, 2, 1},
-                     };
-    int x = 4, y = 4, newC = 3;
-    floodFill(screen, x, y, newC);
- 
-    cout << "Updated screen after call to floodFill: \n";
-    for (int i=0; i<M; i++)
-    {
-        for (int j=0; j<N; j++)
-           cout << screen[i][j] << " ";
-        cout << endl;
+    for (int i = 0; i < N; i++) {
+        string row;
+        cin >> row;
+        for (int j = 0; j < M; j++) {
+            l.SetRoom(i, j, row[j]);
+        }
     }
+    l.floodFill(0, 0, 'A');
+    l.PrintRoom();
+
 }
-//Updated by Arun Pandey
--- HW3-C.md
+++ HW3-C.md
@@ -7,6 +7,17 @@
 2. 要把障礙物當成不能長大的岩漿嗎？
 3. 岩漿的邊緣很多，會算很久怎麼辦？
 
+### Ans
+==岩漿擴散之後用queue把它存起來，多存一個timer去記擴散到第幾輪，把上面兩個包成一個struct。==
+
+1. binary search的做法：
+==隨著岩漿擴散，還有路可以走就存1，開始沒路可以走的時候存成0，用binary search找出什麼時候開始沒有路可以走的。==
+
+![](https://hackmd.io/_uploads/SJo_WTXSn.jpg)
+![](https://hackmd.io/_uploads/B1jOZ67rn.jpg)
+
+2. disjoint set的做法：
+==這個做法聽起來概念跟union find蠻像的，但他沒有講到非常細。==
 
 ![IMG_0222](https://i.imgur.com/0TrUnYQ.png)
 
