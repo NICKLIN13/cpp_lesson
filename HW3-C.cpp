cce738ddf05cf13f4726c225353219dd69764c07
andrewchiull <63439849+andrewchiull@users.noreply.github.com>
2023-05-21 18:21:15 +0800

    HW3-C: Set borders

--- HW3-C.cpp
+++ HW3-C.cpp
@@ -3,14 +3,27 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <sstream>
+#include <queue>
+#include <map>
 
 using namespace std;
 
 bool debug = true;
 
+map<char, char> mp {
+    {'O', '='},
+    {'C', '.'},
+    {'L', 'L'},
+    {'B', 'B'},
+    {'D', 'D'},
+};
+
 struct Block {
+    int x;
+    int y;
     char type;
-    int timer = 0;
+    int timer;
 };
 
 class Lava {
@@ -18,18 +31,21 @@ private:
     const int N, M;
     vector<vector<Block> > room;
     void floodFillUtil(int x, int y, char prevC, char newC);
+    queue<Block> q;
 public:
     Lava(int N, int M): N(N), M(M) {
-        room.resize(N);
-        for (int i = 0; i < N; i++) {
-            room[i].resize(M);
+        room.resize(N+2);
+        for (int i = 0; i < N+2; i++) {
+            room[i].resize(M+2);
         }
     }
-    void SetType(int x, int y, char C);
-    char GetType(int x, int y);
+    void SetBlock(int x, int y, char type, int timer);
+    Block GetBlock(int x, int y);
     bool IsValidPosition(int x, int y);
     void PrintRoom();
     void floodFill(int x, int y, char newC);
+    void EnqueueLava(int x, int y);
+    void SpreadLava(int x, int y, char prevC, char newC);
 
 };
 
@@ -37,21 +53,18 @@ bool Lava::IsValidPosition(int x, int y) {
     return (x >= 0 && x < N && y >= 0 && y < M);
 }
 
-char Lava::GetType(int x, int y) {
-    if (!IsValidPosition(x, y)) return 'X';
-    return room[x][y].type;
+Block Lava::GetBlock(int x, int y) {
+    return room[x+1][y+1];
 }
 
-void Lava::SetType(int x, int y, char c) {
-    if (debug) cout << x << "," << y << " = " << c << "\n";
-    room[x][y].type = c;
-    if (debug) cout << "done\n";
+void Lava::SetBlock(int x, int y, char type, int timer) {
+    room[x+1][y+1] = Block{x, y, type, timer};
 }
 
 void Lava::PrintRoom() {
     for (auto row: room) {
         for (auto col: row) {
-            cout << col.type;
+            cout << mp[col.type];
         }
         cout << "\n";
     }
@@ -63,12 +76,12 @@ void Lava::floodFillUtil(int x, int y, char prevC, char newC)
 {
     if (!IsValidPosition(x, y)) return;
 
-    auto currC = GetType(x, y);
+    auto currC = GetBlock(x, y).type;
     if (currC != prevC) return;
     if (currC == newC) return;
 
     // Replace the color at (x, y)
-    SetType(x, y, newC);
+    SetBlock(x, y, newC, GetBlock(x, y).timer);
 
     // Recur for north, east, south and west
     floodFillUtil(x+1, y, prevC, newC);
@@ -81,15 +94,33 @@ void Lava::floodFillUtil(int x, int y, char prevC, char newC)
 // calls floodFillUtil()
 void Lava::floodFill(int x, int y, char newC)
 {
-    char prevC = GetType(x, y);
+    char prevC = GetBlock(x, y).type;
     if (prevC == newC) return;
     floodFillUtil(x, y, prevC, newC);
+}
+
+void Lava::EnqueueLava(int x, int y) {
+    q.push(room[x][y]);
+}
+
+void Lava::SpreadLava(int x, int y, char prevC, char newC)
+{
+    if (!IsValidPosition(x, y)) return;
+
+    auto currC = GetBlock(x, y).type;
+    if (currC != prevC) return;
+    if (currC == newC) return;
+
+    // Replace the color at (x, y)
+    SetBlock(x, y, newC, GetBlock(x, y).timer);
 }
 
 // Driver code
 int main()
 {
     debug = false;
+    stringstream cin("3 4\nCCCD\nCCCC\nBCLC");
+
     int N, M;
     cin >> N >> M;
     Lava l(N, M);
@@ -98,10 +129,21 @@ int main()
         string row;
         cin >> row;
         for (int j = 0; j < M; j++) {
-            l.SetType(i, j, row[j]);
+            auto type = row[j];
+            l.SetBlock(i, j, type, 0);
         }
+
     }
-    l.floodFill(0, 0, 'A');
+    for (int i = -1; i <= N; i++) {
+        l.SetBlock(i, -1, 'O', 0);
+        l.SetBlock(i, M, 'O', 0);
+    }
+    for (int j = -1; j <= M; j++) {
+        l.SetBlock(-1, j, 'O', 0);
+        l.SetBlock(N, j, 'O', 0);
+    }
+
+
     l.PrintRoom();
 
 }
--- test.cpp
+++ test.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
 using namespace std;
 
+struct Test
+{
+    int N, M;
+};
+
+
 int main(){
-    long long N, M;
+    int N, M;
     cout << "Input N and M: ";
     cin >> N >> M;
 
     cout << N << " " << M << "\n";
+    Test t = {N, M};
+    cout << t.N << " " << t.M << "\n";
     return 0;
 }
