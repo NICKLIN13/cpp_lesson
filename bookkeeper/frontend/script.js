e8871d37ed5876df3e9cc3b6a4460b3045066030
NICKLIN <et20300051@gmail.com>
2023-11-10 09:37:19 +0800

    HW-page without fuction

---
+++ bookkeeper/frontend/script.js
@@ -0,0 +1,13 @@
+
+
+function addEventListener() {
+    const headingInput = document.getElementById("heading-input");
+    const executeButton = document.getElementById("change-heading");
+
+    executeButton.addEventListener("click", function() {
+        const title = headingInput.value;
+        document.getElementById("heading").textContent = title;
+    });
+}
+
+addEventListener();
