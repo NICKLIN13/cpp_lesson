c8679f1834a4f1321acd4dc914e4f37c17e40456
NICKLIN <et20300051@gmail.com>
2024-03-15 15:29:16 +0800

    todolist-react model done

--- todolist-react/src/components/EditTodoForm.js
+++ todolist-react/src/components/EditTodoForm.js
@@ -1,10 +1,24 @@
 import React from 'react'
+import { useState } from 'react'
 
-const EditTodoForm = () => {
+const EditTodoForm = ({editTodo, task}) => {
+    const [value, setValue] = useState(task.task)
+
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        editTodo(value, task.id);
+        setValue("")
+    }
   return (
-    <div>
-      
-    </div>
+    <form className='EditTodoForm' onSubmit={handleSubmit}>
+        <input type='text' className='todo-input' 
+        value={value}
+        placeholder='Update Task'
+        onChange={(e) => setValue(e.target.value)} />
+        <button type='submit' className='todo-btn'>Update Task</button>
+    </form>
+
   )
 }
 
