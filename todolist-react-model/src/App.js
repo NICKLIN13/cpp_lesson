f9ad334c06bf6ac7b422805eddf5952940954e34
NICKLIN <et20300051@gmail.com>
2024-03-15 15:47:12 +0800

    just rename a file

--- todolist-react-model/src/App.js
+++ todolist-react-model/src/App.js
@@ -2,11 +2,11 @@ import './App.css';
 import TodoWrapper from './components/TodoWrapper';
 
 function App() {
-  return (
-    <div className="App">
-      <TodoWrapper />
-    </div>
-  );
+	return (
+		<div className="App">
+			<TodoWrapper />
+    	</div>
+	);
 }
 
 export default App;
