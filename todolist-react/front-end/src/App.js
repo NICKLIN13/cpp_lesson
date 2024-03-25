7787df83a481734eaac976ea020e36c58ef8e536
NICKLIN <et20300051@gmail.com>
2024-03-25 21:15:28 +0800

    Before class

--- todolist-react/front-end/src/App.js
+++ todolist-react/front-end/src/App.js
@@ -1,18 +1,22 @@
+import { prefix } from '@fortawesome/free-solid-svg-icons';
 import './App.css';
 import TodoWrapper from './components/TodoWrapper';
 
 import { useEffect } from 'react';
+import { useState } from 'react'; // 這只是新增試試看能不能串連後端的
 
 
 function App() {
+	const [todos, setTodos] = useState([]); // 這只是新增試試看能不能串連後端的
 
 	useEffect(() => {
 		async function getTodos() {
 			const res = await fetch("/api/todos");
 			const todos = await res.json();
 
-			console.log(todos);
+			setTodos(todos); // 這只是新增試試看能不能串連後端的
 		}
+		
 		getTodos();
 	}, [])
 
@@ -20,6 +24,7 @@ function App() {
 	return (
 		<div className="App">
 			<TodoWrapper />
+			<pre>{JSON.stringify(todos, null, 2)}</pre> // 這只是新增試試看能不能串連後端的
     	</div>
 	);
 }
