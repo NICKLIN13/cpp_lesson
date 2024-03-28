98f270eb1e1e8a4a251bf6acad0e02d644a62445
NICKLIN <et20300051@gmail.com>
2024-03-28 22:23:02 +0800

    Use axios instead of fetch

--- todolist-react-tryingBE/front-end/src/App.js
+++ todolist-react-tryingBE/front-end/src/App.js
@@ -5,26 +5,42 @@ import TodoWrapper from './components/TodoWrapper';
 import { useEffect } from 'react';
 import { useState } from 'react'; // 這只是新增試試看能不能串連後端的
 
+import axios from "axios";
+
+
+const instance = axios.create({
+	baseURL: "http://localhost:5001/api"
+});
+
+async function getTodos() {
+	const res = await instance.get("/todos");
+	console.log("res")
+	console.log(res)
+	return JSON.stringify(res.data);
+}
+
+
+
 
 function App() {
-	const [todos, setTodos] = useState([]); // 這只是新增試試看能不能串連後端的
+	const [todos, setTodos] = useState(); // 這只是新增試試看能不能串連後端的
 
 	useEffect(() => {
-		async function getTodos() {
-			const res = await fetch("/api/todos");
-			const todos = await res.json();
-
-			setTodos(todos); // 這只是新增試試看能不能串連後端的
+		const init = async () => {
+			const data = await getTodos();
+			console.log("in useEffect");
+			console.log(data);
+			setTodos(data)
 		}
 		
-		getTodos();
+		init()
 	}, [])
 
 
 	return (
 		<div className="App">
 			<TodoWrapper />
-			<pre>{JSON.stringify(todos, null, 2)}</pre> // 這只是新增試試看能不能串連後端的
+			<pre>{JSON.stringify(todos)}</pre> // 這只是新增試試看能不能串連後端的
     	</div>
 	);
 }
