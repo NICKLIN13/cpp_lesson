65a9a227a462b57a8953389495eac1ea7a73b0b7
NICKLIN <et20300051@gmail.com>
2024-04-03 10:24:53 +0800

    deleteTodo done

--- 2024-04-02-fullstack-MERN-todolist/client/src/Todo.jsx
+++ 2024-04-02-fullstack-MERN-todolist/client/src/Todo.jsx
@@ -4,7 +4,7 @@ export default function Todo(props) {
     const { todo, setTodos } = props;
 
     // a function that changes the status of check
-    const updateTodo = async (todoId, todoStatus) => { 
+    const updateTodo = async (todoId, todoStatus) => {
         const res = await fetch(`/api/todos/${todoId}`, {
             method: "PUT",
             body: JSON.stringify({ status: todoStatus }),
@@ -26,6 +26,20 @@ export default function Todo(props) {
         }
     };
 
+    const deleteTodo = async (todoId) => {
+        const res = await fetch(`/api/todos/${todoId}`, {
+            method: "DELETE"
+        });
+        const json = await res.json();
+        if (json.acknowledged) {
+            setTodos(currentTodos => {
+                return currentTodos
+                .filter((currentTodo) => (currentTodo._id !== todoId));
+            })
+        }
+
+    };
+
     return (
         <div className="todo">
             <p>{todo.todo}</p>
@@ -35,6 +49,12 @@ export default function Todo(props) {
                     onClick={() => updateTodo(todo._id, todo.status)}
                 >
                     {(todo.status) ? "☑" : "☐"}
+                </button>
+                <button
+                    className="todo__delete"
+                    onClick={() => deleteTodo(todo._id)}
+                >
+                    🗑️
                 </button>
             </div>
         </div>
